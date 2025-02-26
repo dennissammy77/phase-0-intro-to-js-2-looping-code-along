324c227b0583ba097ed20dda762b640874ed97c4
dennissammy77 <dennissammy77@gmail.com>
2025-02-26 09:30:53 +0300

    Working with loops

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solutions in this file
+const gifts = ['teddy bear', 'drone', 'doll'];
+
+function wrapGifts(gifts){
+  for(let i = 0; i < gifts?.length; i++){
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+  return gifts
+};
+
+wrapGifts(gifts)
+
+function writeCards(namesarray,eventname){
+  let temparr = [];
+  for(let i = 0; i < namesarray?.length; i++){
+    temparr.push(`Thank you, ${namesarray[i]}, for the wonderful ${eventname} gift!`)
+  };
+  return temparr;
+}
+
+
+function wrapGiftsWhileLoop(array){
+  let i = 0;
+  while (i < array?.length) {
+    console.log(`Wrapped ${array[i]} and added a bow!`);
+    i++;
+  }
+  return array;
+}
+
+wrapGiftsWhileLoop(gifts)
+
+function countDown(positiveInteger){
+  while(positiveInteger >= 0){
+    console.log(positiveInteger)
+    positiveInteger--
+  }
+  return;
+}
+
